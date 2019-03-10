b41c37edf6c4d6504302cd052432a414c0b9c27e
Nilton Paiva <nfrpaiva@gmail.com>
2019-03-10 14:27:12 -0300

    Fix para funcionar no firefox. Um pouco mais de estilo

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -13,12 +13,15 @@
         </tr>
       </tbody>
     </table>
-    <div>
+    <fieldset :disabled="aguardando">
       <button class="btn btn-primary" @click="handleDownloadFetch">Download using fetch</button>
       
       <button class="btn btn-success" @click="handleDownloadAxios">Download usin Axios</button>
       
       <button class="btn btn-warning" @click="handleDownloadHRef">Download usin HRef</button>
+    </fieldset>
+    <div class="aguarde">
+      <h1>Aguarde</h1>
     </div>
   </div>
 </template>
@@ -29,6 +32,15 @@ export default {
   name: "ProductList",
   components: {},
   methods: {
+    aguarde(enable) {
+      if (enable) {
+        document.querySelector(".aguarde").style.display = "block";
+        this.aguardando = true;
+      } else {
+        document.querySelector(".aguarde").style.display = "none";
+        this.aguardando = false;
+      }
+    },
     getHeaderFileName(contentDisposition) {
       if (!contentDisposition) {
         return "defaultname.pdf";
@@ -42,26 +54,36 @@ export default {
       const el = document.createElement("a");
       el.href = window.URL.createObjectURL(blob);
       el.download = filename;
+      //Necessário para funcionar no Firefox
+      document.body.appendChild(el);
       el.click();
+      //Importante remover o elemento para não consumir/reter muita memória
+      el.remove();
     },
     handleDownloadHRef() {
       window.location.assign("/pdf/spring-boot-reference.pdf");
     },
     handleDownloadAxios() {
+      this.aguarde(true);
       axios
         .get("/pdf/spring-boot-reference.pdf", {
           responseType: "arraybuffer"
         })
         .then(res => {
           let header = res.headers["content-disposition"] || "";
-          console.log("headerx", header);
           let filename = this.getHeaderFileName(header);
           let blob = new Blob([res.data], { type: "application/pdf" });
           this.createElementWithBlob(blob, filename);
         })
-        .catch();
+        .catch(e => console.log(e))
+        .finally(() => {
+          console.log("finally");
+          this.aguarde(false);
+        });
     },
     handleDownloadFetch() {
+      this.aguarde(true);
+      document.querySelector(".aguarde").style.display = "block";
       let filename;
       fetch("/pdf/spring-boot-reference.pdf")
         .then(res => {
@@ -72,11 +94,16 @@ export default {
         .then(blob => {
           this.createElementWithBlob(blob, filename);
         })
-        .catch(e => console.log(e));
+        .catch(e => console.log(e))
+        .finally(() => {
+          console.log("finally");
+          this.aguarde(false);
+        });
     }
   },
   data() {
     return {
+      aguardando: false,
       dados: [
         {
           id: 1,
@@ -98,5 +125,21 @@ export default {
 table {
   width: 60vw;
   margin: 0% auto;
+}
+.aguarde {
+  display: none;
+  position: fixed;
+  border: 1px solid #a2a2a2;
+  width: 200px;
+  height: 54px;
+  text-align: center;
+
+  top: 50vh;
+  left: 50vw;
+  margin-top: -27px;
+  margin-left: -100px;
+  background-color: white;
+  border-radius: 6px;
+  box-shadow: 2px 2px 2px rgb(0, 0, 0, 0.25);
 }
 </style>
